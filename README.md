3eb680226a9cf86045e8abd74dddae331dbdbdc9
fiiv <git@fiiv.io>
2019-01-14 09:56:26 +0700

    project skeleton

---
+++ README.md
@@ -0,0 +1,3 @@
+# Blogsend.io Wordpress plugin
+
+This helps speed up integration with Wordpress + Blogsend.io
---
+++ blogsend.php
@@ -0,0 +1,4 @@
+<?
+
+  // Stop file from being run directly
+  defined('ABSPATH') or die('No direct access to this file permitted.');
